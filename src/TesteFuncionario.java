c3fd0b993ac5b3c75f8e8350f24f151ffdd0c86f
luis.pires <luispires.m@gmail.com>
2022-02-13 22:36:44 -0300

    primeiro commit

---
+++ src/TesteFuncionario.java
@@ -0,0 +1,16 @@
+
+public class TesteFuncionario {
+
+	public static void main(String[] args) {
+
+
+		Funcionario luis = new Funcionario();
+		luis.setNome("Luis Pires");
+		luis.setCpf("00722252030");
+		luis.setSalario(2888.00);
+		System.out.println(luis);
+		
+
+	}
+
+}
